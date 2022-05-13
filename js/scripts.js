89b89c746ca4c2fca34336a5212d6423ab640135
arkyvio <arkyvio@gmail.com>
2022-05-13 14:12:34 -0400

    Fixed signup div centering, Sped up animations.

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,19 @@ $(document).ready(function () {
     $('#homebutton').click(function () {
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".hero-container").offset().top
-        }, 2000);
+        }, 1000);
     });
 
     $('#infobutton').click(function () {
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".info-container").offset().top
-        }, 2000);
+        }, 1000);
     });
 
     $('#signbutton').click(function () {
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".copy-container").offset().top
-        }, 2000);
+        }, 1000);
     });
 
 });
